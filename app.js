c8cec09ace1a8ead48f6407ebc67b178e67bcf61
univertaavi-khk <univer.taavi@khk.ee>
2021-10-15 14:28:02 +0300

    1. testi ülesanne

---
+++ app.js
@@ -0,0 +1,28 @@
+class Shape {
+	constructor(name, sides, sideLength) {
+		this.name = name;
+		this.sides = sides;
+		this.sideLength = sideLength;
+	}
+	calcPerimeter(){
+		return this.sides * this.sideLength;
+	}
+}
+
+class Square extends Shape {
+	calcArea(){
+		return this.sideLength * this.sideLength;
+	}
+}
+
+class Triangle extends Shape {
+	calcPerimeter(){
+		return this.sides * this.sideLength;
+	}	
+}
+
+
+const square = new Square("square", 4, 5);
+const triangle = new Triangle("triangle", 3, 3)
+console.log(square.calcPerimeter());
+console.log(triangle.calcPerimeter())
